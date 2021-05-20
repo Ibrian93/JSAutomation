f12f0c5527338741c1e7c3b240c417027dc13ae9
Brian Icochea <ibrian93@gmail.com>
2021-05-20 23:12:24 +0200

    Authorization with Chai

---
+++ controllers/models/user.js
@@ -0,0 +1,8 @@
+class User {
+    constructor(userName, password) {
+        this.userName = userName;
+        this.password = password;
+    }
+}
+
+module.exports = User;
--- test/authorized.js
+++ test/authorized.js
@@ -1,16 +1,18 @@
+const User = require('../controllers/models/user.js');
+
+
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let should = chai.should();
 let host = "https://demoqa.com";
 
+
 chai.use(chaiHttp);
 
 describe('The given user is authorized', () => {
     it('The default user should be authorized', (done) => {
-        let defaultUser = {
-            userName: "ibrian93",
-            password: "MyTesting83!"
-        }
+
+        const defaultUser = new User("ibrian93", "MyTesting83!");
         chai.request(host)
             .post('/Account/v1/Authorized')
             .send(defaultUser)
