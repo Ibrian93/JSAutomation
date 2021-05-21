0409faad8226912bbb99920fc7b01a4489baa5ca
Brian Icochea <ibrian93@gmail.com>
2021-05-21 10:25:13 +0200

    adding test for authorized user

--- test/authorized.js
+++ test/authorized.js
@@ -4,7 +4,7 @@ const User = require('../controllers/models/user.js');
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let should = chai.should();
-let host = "https://demoqa.com";
+let config = require('../config/environment.json');
 
 
 chai.use(chaiHttp);
@@ -13,11 +13,12 @@ describe('The given user is authorized', () => {
     it('The default user should be authorized', (done) => {
 
         const defaultUser = new User("ibrian93", "MyTesting83!");
-        chai.request(host)
+        chai.request(config.host)
             .post('/Account/v1/Authorized')
             .send(defaultUser)
             .end((err, res) => {
                 res.should.have.status(200);
+                res.body.should.equal(true);
                 done();
             })
     });
