471a928c4c577c5468358ad59144f10dfafd1dc1
Brian Icochea <ibrian93@gmail.com>
2021-05-21 22:36:39 +0200

    Adding null password test case

--- test/user-registration.js
+++ test/user-registration.js
@@ -4,18 +4,29 @@ const config = require('../config/environment.json');
 const request = require('supertest')(config.host);
 const expect = require('chai').expect;
 var faker = require('faker');
-const { response } = require('express');
 
+
+var accountEndpoint = "/Account/v1/User";
 
 describe('Account API, User endpoint', () => {
     describe('User Registration Successful', () => {
         it('The user who meets the proper criterias, should be successfully register', async function() {
-            let defaultUser = new User(faker.internet.email(), "MyTesting83!");
-            let response = await request.post('/Account/v1/User')
+            let defaultUser = new User(faker.internet.email(), 'MyTesting83!');
+            let response = await request.post(accountEndpoint)
                 .send(defaultUser);
             expect(response.status).to.be.eql(201);
             expect(response.body.userID).to.not.be.undefined;
             expect(response.body.username).to.be.eql(defaultUser.userName);
+        });
+    });
+    describe('User registration not successful', () => {
+        it('The user with no password criteria should not be able to register', async function() {
+            let user = new User(faker.internet.email());
+            let response = await request.post(accountEndpoint)
+                .send(user);
+            expect(response.status).to.be.eql(400);
+            expect(response.body.code).to.be.eql('1200');
+            expect(response.body.message).to.be.eql('UserName and Password required.');
         })
-    })
+    });
 })
