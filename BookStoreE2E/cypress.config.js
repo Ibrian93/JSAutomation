1bb2685fee4f5f5e8dd904b3ab3d83b08c8fa75e
Brian Icochea <ibrian93@gmail.com>
2023-01-25 22:04:06 +0100

    Modifying cypress config

--- BookStoreE2E/cypress.config.js
+++ BookStoreE2E/cypress.config.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD:BookStoreE2E/cypress.config.js
 module.exports = {
-=======
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
->>>>>>> da8de033c1f7b93eb0370c298f49e4c95a32040c:Frontend/cypress.config.js
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -12,8 +6,4 @@ module.exports = defineConfig({
       return require('./cypress/plugins/index.js')(on, config)
     },
   },
-<<<<<<< HEAD:BookStoreE2E/cypress.config.js
 }
-=======
-})
->>>>>>> da8de033c1f7b93eb0370c298f49e4c95a32040c:Frontend/cypress.config.js
